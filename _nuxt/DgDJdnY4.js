00537b5bc9be1113d08646fc907521eb6e9cb98e
Monika Bohn <monika.bohn@eduhouse.fi>
2025-06-23 19:55:05 +0200

    Updates

---
+++ _nuxt/DgDJdnY4.js
@@ -0,0 +1,1 @@
+import{P as n,r as o}from"./BqdNcEs2.js";const i=n("signup",()=>{const e=o("");function r(t){e.value=t}return{email:e,setEmail:r}});export{i as u};
--- stores/types/types.ts
+++
@@ -1,9 +0,0 @@
-export interface FormData {
-    email: string
-    password: string,
-    productUpdates: boolean
-}
-export interface FormErrors {
-    email: string
-    password: string
-}
